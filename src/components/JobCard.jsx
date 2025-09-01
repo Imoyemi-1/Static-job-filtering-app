984cff81264c663eaa4cf08a2fa7ac7b7b3a19bd
Imoyemi <okeibrahim133@gmail.com>
2025-09-01 18:20:14 -0500

    Fix: fixed filter rerendering bug

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,4 +1,4 @@
-function JobCard({ job, setJobFilter, setFilteredJob }) {
+function JobCard({ job, setFilteredJob }) {
   return (
     <article className='relative bg-white w-full p-6 rounded-lg max-w-[1024px] shadow-md flex flex-col md:flex-row md:items-center md:justify-between gap-4 border-l-4 border-transparent hover:border-green-c-400 transition-colors duration-300 ease-in-out'>
       <img
@@ -32,10 +32,7 @@ function JobCard({ job, setJobFilter, setFilteredJob }) {
       <ul className='flex gap-4 flex-wrap text-green-c-400 font-bold '>
         <li
           onClick={() => {
-            setJobFilter((prev) =>
-              prev.includes(job.role) ? prev : [...prev, job.role]
-            );
-            setFilteredJob();
+            setFilteredJob(job.role);
           }}
           className='bg-green-c-50 px-3 py-1 rounded shadow-sm hover:bg-green-c-400 hover:text-green-c-50 transition-colors duration-300 ease-in-out cursor-pointer'
         >
@@ -43,10 +40,7 @@ function JobCard({ job, setJobFilter, setFilteredJob }) {
         </li>
         <li
           onClick={() => {
-            setJobFilter((prev) =>
-              prev.includes(job.level) ? prev : [...prev, job.level]
-            );
-            setFilteredJob();
+            setFilteredJob(job.level);
           }}
           className='bg-green-c-50 px-3 py-1 rounded shadow-sm hover:bg-green-c-400 hover:text-green-c-50 transition-colors duration-300 ease-in-out cursor-pointer'
         >
@@ -56,10 +50,7 @@ function JobCard({ job, setJobFilter, setFilteredJob }) {
           job.languages.map((lang) => (
             <li
               onClick={() => {
-                setJobFilter((prev) =>
-                  prev.includes(lang) ? prev : [...prev, lang]
-                );
-                setFilteredJob();
+                setFilteredJob(lang);
               }}
               key={lang}
               className='bg-green-c-50 px-3 py-1 rounded shadow-sm hover:bg-green-c-400 hover:text-green-c-50 transition-colors duration-300 ease-in-out cursor-pointer'
@@ -71,10 +62,7 @@ function JobCard({ job, setJobFilter, setFilteredJob }) {
           job.tools.map((tool) => (
             <li
               onClick={() => {
-                setJobFilter((prev) =>
-                  prev.includes(tool) ? prev : [...prev, tool]
-                );
-                setFilteredJob();
+                setFilteredJob(tool);
               }}
               key={tool}
               className='bg-green-c-50 px-3 py-1 rounded shadow-sm hover:bg-green-c-400 hover:text-green-c-50 transition-colors duration-300 ease-in-out cursor-pointer'
--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -3,27 +3,23 @@ import { useState } from 'react';
 import data from '/data.json';
 
 function MainPage() {
-  const [jobData, setJobData] = useState(data);
   const [jobFilter, setJobFilter] = useState([]);
 
-  const filteredJobs = jobData.filter((job) => {
+  const filteredJobs = data.filter((job) => {
     const tags = [job.role, job.level, ...job.languages, ...job.tools];
 
     return jobFilter.every((f) => tags.includes(f));
   });
 
   //   set filter job to job data
-  const setFilteredJob = () => setJobData(filteredJobs);
+  const setFilteredJob = (word) => {
+    setJobFilter((prev) => (prev.includes(word) ? prev : [...prev, word]));
+  };
 
   return (
     <main className='flex flex-col md:items-center  gap-y-14 p-6 md:gap-y-6 pt-15 bg-green-c-50 min-h-screen'>
-      {jobData.map((job) => (
-        <JobCard
-          key={job.id}
-          job={job}
-          setJobFilter={setJobFilter}
-          setFilteredJob={setFilteredJob}
-        />
+      {filteredJobs.map((job) => (
+        <JobCard key={job.id} job={job} setFilteredJob={setFilteredJob} />
       ))}
     </main>
   );
