c421ccf64d55f4136a4c111da3a76a9aa4d7d04f
Imoyemi <okeibrahim133@gmail.com>
2025-09-02 07:21:36 -0500

    feat: added filter bar component

--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -1,9 +1,16 @@
 import JobCard from './JobCard';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import data from '/data.json';
 
 function MainPage() {
   const [jobFilter, setJobFilter] = useState([]);
+  const [filterHeight, setFilterHeight] = useState(0);
+
+  // refs for filter div and main div
+  const filterRef = useRef(null);
+  const mainRef = useRef(null);
+
+  // filter job data based on jobFilter state array
 
   const filteredJobs = data.filter((job) => {
     const tags = [job.role, job.level, ...job.languages, ...job.tools];
@@ -16,11 +23,57 @@ function MainPage() {
     setJobFilter((prev) => (prev.includes(word) ? prev : [...prev, word]));
   };
 
+  useEffect(() => {
+    // set the height of the filter bar to a CSS variable
+    if (filterRef.current) {
+      const height = filterRef.current.offsetHeight;
+      setFilterHeight(height);
+      mainRef.current.style.paddingTop = `${height + 32}px`;
+    }
+
+    window.addEventListener('resize', () => {
+      if (filterRef.current) {
+        const height = filterRef.current.offsetHeight;
+        setFilterHeight(height);
+        mainRef.current.style.paddingTop = `${height + 32}px`;
+      }
+    });
+
+    return () => {
+      window.removeEventListener('resize', () => {});
+    };
+  }, [filterHeight, jobFilter]);
+
   return (
-    <main className='flex flex-col md:items-center  gap-y-14 p-6 md:gap-y-6 pt-15 bg-green-c-50 min-h-screen'>
+    <main
+      ref={mainRef}
+      className='relative h-fit flex flex-col md:items-center  gap-y-14 p-6 md:gap-y-6  pt-15 bg-green-c-50 min-h-screen'
+    >
       {filteredJobs.map((job) => (
         <JobCard key={job.id} job={job} setFilteredJob={setFilteredJob} />
       ))}
+      {!jobFilter.length ? null : (
+        <div
+          ref={filterRef}
+          className='absolute -top-8 left-[1.5rem] right-[1.5rem] p-4 flex items-center justify-between bg-white rounded-lg max-w-[1024px] mx-auto shadow-md'
+        >
+          <div className='flex gap-3 flex-wrap'>
+            {jobFilter.map((job) => (
+              <div className='flex ' key={job}>
+                <p className='bg-green-c-50 px-3 py-1 rounded-bl-sm rounded-tl-sm shadow-sm font-medium text-green-c-400 '>
+                  {job}
+                </p>
+                <button className='p-1 bg-black rounded-br-sm rounded-tr-sm shadow-sm px-2'>
+                  <img src='/images/icon-remove.svg' alt='remove-icon' />
+                </button>
+              </div>
+            ))}
+          </div>
+          <button className='text-gray-c-400 font-medium hover:text-green-c-400 hover:underline underline-offset-1 transition-colors duration-300 cursor-pointer'>
+            Clear
+          </button>
+        </div>
+      )}
     </main>
   );
 }
